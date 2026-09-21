e5ed3cb2becca773f448568f0e753c460563c802
PARTHRAJSINHgohil007 <gohilparthrajsinhgohil065@gmail.com>
2026-09-21 18:28:02 +0530

    Time: 1 ms (5.93%), Space: 11 MB (13.78%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    void solve(TreeNode* root) {
+        if (root == NULL)
+            return;
+
+        solve(root->left);
+        ans.push_back(root->val);
+        solve(root->right);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+};
