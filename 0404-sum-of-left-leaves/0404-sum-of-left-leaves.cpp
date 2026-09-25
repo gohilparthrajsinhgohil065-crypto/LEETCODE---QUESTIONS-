39703ffe84309f48ffc0f987ae575af1b4bc5ea9
PARTHRAJSINHgohil007 <gohilparthrajsinhgohil065@gmail.com>
2026-09-25 17:51:08 +0530

    Time: 0 ms (100%), Space: 16.3 MB (22.72%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        if(root->left != NULL) {
+            if(root->left->left == NULL && root->left->right == NULL) {
+                sum += root->left->val;
+            }
+            else {
+                sum += sumOfLeftLeaves(root->left);
+            }
+        }
+
+        sum += sumOfLeftLeaves(root->right);
+
+        return sum;
+    }
+};
